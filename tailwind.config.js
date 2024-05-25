95259671a0a03c6876d59d4d83bfccec85528a89
MUHAMMAD ABDULLAH <135140627+mr-abd21@users.noreply.github.com>
2024-05-25 17:32:34 +0500

    Portfolio Completed

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {backgroundColor: {
+      'primary': '#040e45',
+    }},
+  },
+  plugins: [],
+}
+
